26fa252d299bcf1f7d4242fbcfc4bb9b95152127
Prashant Shrestha <prashantlurvs@gmail.com>
2025-07-09 06:49:33 +0200

    Podfile updated

--- ios/EncodeLinkSdk.m
+++ ios/EncodeLinkSdk.m
@@ -1,4 +1,4 @@
-#import "React/RCTBridgeModule.h"
+@import "React/RCTBridgeModule.h"
 @interface RCT_EXTERN_MODULE(EncodeLinkSdk, NSObject)
 RCT_EXTERN_METHOD(show:(NSString *)token callback:(RCTResponseSenderBlock)callback)
 @end
